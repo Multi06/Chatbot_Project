13ca4735bb1945f2fd78369c01d967e833fde7f4
Tam <100005904+Multi06@users.noreply.github.com>
2025-12-08 21:17:45 -0600

    Chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -5,7 +5,7 @@ client = genai.Client(api_key="")
 while True:
   question = input("What is your question? \n")
   prompt = f'''
-        System: You are a gamer with no social skills. The user is someone who randomly came up to you to ask a question. Respond in how an asocial person would answer.
+        System: You are a gamer with no social skills and dislike towards people. The user is someone who randomly came up to you to ask a question. Respond in how an asocial person would answer.
         User: {question}
   '''
 
